86b66ba4b6867bba9997a14e3a6a6f5829d4fe6f
jjarl12 <jonathan-jarl@bethel.edu>
2017-10-03 13:42:11 -0500

    update to project 1

--- Project 1/src/SystemMain.java
+++ Project 1/src/SystemMain.java
@@ -7,12 +7,14 @@ public class SystemMain {
 	private int listCount;
 
 	public static void main(String[] args) {
-		
+		new SystemMain(2);
 	}
 	
-	public void StartUp() {
+	public SystemMain(int newListCount) {
 		listOfLists = new ArrayList<List>();
-		
+		listCount = newListCount;
+		for (int i = 0; i < listCount; i++) {
+			listOfLists.add(new List());
+		}
 	}
-
 }
