815b449a497ea9b5f5a6a01ab703996ad07dd372
jjarl12 <jonathan-jarl@bethel.edu>
2017-10-20 15:42:28 -0500

    remove crap

--- GUI Practice/src/Controller.java
+++
@@ -1,9 +0,0 @@
-
-public class Controller {
-
-	public static void main(String[] args) {
-		View gui = new View();
-
-	}
-
-}
--- GUI Practice/src/View.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class View extends JFrame{
-	JFrame mainFrame = new JFrame();
-	JPanel mainPanel = new JPanel();
-}
